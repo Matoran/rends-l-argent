83a3be6fbf27b6614fb0a1333e6c33827a596f9d
matoran <marco.rodrigues-lopes@etu.hesge.ch>
2017-09-08 10:58:26 +0200

    refactor code, create "packages", add page configuration then play

---
+++ js/display.js
@@ -0,0 +1,193 @@
+"use strict";
+
+function strcutSquare(posx, posy, w, h, color, id, dx, dy, cell) {
+    return {
+        posx: posx,
+        posy: posy,
+        width: w,
+        heigth: h,
+        color: color,
+        id: id,
+        directionX: dx,
+        directionY: dy,
+        cell: cell
+    };
+}
+
+function square(squareParam) {
+    let square = new createjs.Shape();
+    let commandSquare = square.graphics.beginFill("black").command;
+    square.graphics.drawRect(0, 0, squareParam.width, squareParam.heigth);
+    square.x = squareParam.posx;
+    square.y = squareParam.posy;
+    commandSquare.style = squareParam.color;
+    if (squareParam.id !== 0 && squareParam.id !== 10 && squareParam.id !== 20 && squareParam.id !== 30) {
+        square.on("click", function () {
+            squareOnClick(squareParam.id);
+        });
+    }
+    let label = new createjs.Text("", "12px Arial", "#000000");
+    stage.addChild(square);
+    return {
+        square: square,
+        commandeSquare: commandSquare,
+        label: label,
+        struct: squareParam,
+        rectangle: undefined,
+        circle: undefined,
+        img: undefined
+    };
+}
+
+function circle(posx, posy, r, color) {
+    let circle = new createjs.Shape();
+    let commandeCircle = circle.graphics.beginFill(color).command;
+    circle.graphics.drawCircle(0, 0, r);
+    circle.x = posx;
+    circle.y = posy;
+    stage.addChild(circle);
+    return {
+        circle: circle,
+        commandCircle: commandeCircle
+    };
+}
+
+function image(src, posx, posy) {
+    let img = new Image();
+    img.src = src;
+    img.onload = function (event) {
+        let image = event.target;
+        let bitmap = new createjs.Bitmap(image);
+        bitmap.x = posx;
+        bitmap.y = posy;
+        stage.addChild(bitmap);
+        stage.update();
+    };
+    return img;
+}
+
+function text(txt, posx, posy, id) {
+    tabSquare[id].label.text = txt;
+    tabSquare[id].label.x = posx;
+    tabSquare[id].label.y = posy;
+    tabSquare[id].label.alpha = 0.5;
+    stage.addChild(tabSquare[id].label);
+    stage.update();
+}
+
+//draw background
+let background = new createjs.Shape();
+background.graphics.beginFill("#79BD9A").drawRect(0, 0, 700, 700);
+stage.addChild(background);
+
+
+//draw board
+let tabSquare = [];
+let color;
+const squareSize = 90;
+const rectangleWidth = 58;
+let y = 0;
+let x = 0;
+let directionX = 1;
+let directionY = 0;
+for (let i = 0; i < 40; i++) {
+    let cell = board.squares()[i];
+    if (i % 2 === 0) {
+        color = "#3B8686";
+    } else {
+        color = "#0B486B";
+    }
+
+    if (i % 10 === 0) {
+        if (directionX === -1) {
+            x -= squareSize - rectangleWidth;
+        }
+        tabSquare.push(square(strcutSquare(x, y, squareSize, squareSize, color, i, directionX, directionY, cell)));
+        color = "black";
+        text(cell.name(), x, y + 20, i);
+        if (directionX === 1 && i !== 0) {
+            directionX = 0;
+            directionY = 1;
+            y += squareSize;
+        } else if (directionY === 1) {
+            directionX = -1;
+            directionY = 0;
+            x -= rectangleWidth;
+        } else if (directionX === -1) {
+            directionX = 0;
+            directionY = -1;
+            y -= rectangleWidth;
+        } else if (directionX === 1) {
+            x += squareSize;
+        }
+    } else {
+        if (directionX === 1 || directionX === -1) {
+            tabSquare.push(square(strcutSquare(x, y, rectangleWidth, squareSize, color, i, directionX, directionY, cell)));
+            if (cell.isBuyable()) {
+                if (directionX === 1) {
+                    color = cell.owner.color();
+                    tabSquare[i].circle = circle(x + rectangleWidth / 2, y + squareSize + 10, 5, color);
+                } else {
+                    color = cell.owner.color();
+                    tabSquare[i].circle = circle(x + rectangleWidth / 2, y - 10, 5, color);
+                }
+            }
+            if (cell.isProperty()) {
+                if (directionX === 1) {
+                    color = cell.color();
+                    tabSquare[i].rectangle = square(strcutSquare(x + 10, y + squareSize - 20, rectangleWidth - 20, 10, color, i, directionX, directionY, cell));
+                } else {
+                    color = cell.color();
+                    tabSquare[i].rectangle = square(strcutSquare(x + 10, y + 10, rectangleWidth - 20, 10, color, i, directionX, directionY, cell));
+                }
+            } else if (cell.isTrain()) {
+                tabSquare[i].img = image("img/train.png", x, y);
+            } else if (cell.isChance()) {
+                tabSquare[i].img = image("img/chance.png", x, y);
+            } else if (cell.isCommunityChest()) {
+                tabSquare[i].img = image("img/chest.png", x, y);
+            } else if (cell.isTax()) {
+                tabSquare[i].img = image("img/tax.png", x, y);
+            }
+
+            x += rectangleWidth * directionX;
+            y += squareSize * directionY;
+
+        } else if (directionY === 1 || directionY === -1) {
+            tabSquare.push(square(strcutSquare(x, y, squareSize, rectangleWidth, color, i, directionX, directionY, cell)));
+            if (cell.isBuyable()) {
+                if (directionY === 1) {
+                    color = cell.owner.color();
+                    tabSquare[i].circle = circle(x - 10, y + rectangleWidth / 2, 5, color);
+                } else {
+                    color = cell.owner.color();
+                    tabSquare[i].circle = circle(x + squareSize + 10, y + rectangleWidth / 2, 5, color);
+                }
+            }
+            if (cell.isProperty()) {
+                if (directionY === 1) {
+                    color = cell.color();
+                    tabSquare[i].rectangle = square(strcutSquare(x + 10, y + 10, 10, rectangleWidth - 20, color, i, directionX, directionY, cell));
+                } else {
+                    color = cell.color();
+                    tabSquare[i].rectangle = square(strcutSquare(x + squareSize - 20, y + 10, 10, rectangleWidth - 20, color, i, directionX, directionY, cell));
+                }
+            } else if (cell.isTrain()) {
+                tabSquare[i].img = image("img/train.png", x, y);
+            } else if (cell.isChance()) {
+                tabSquare[i].img = image("img/chance.png", x, y);
+            } else if (cell.isCommunityChest()) {
+                tabSquare[i].img = image("img/chest.png", x, y);
+            } else if (cell.isTax()) {
+                tabSquare[i].img = image("img/tax.png", x, y);
+            }
+            x += squareSize * directionX;
+            y += rectangleWidth * directionY;
+        }
+    }
+}
+
+tabSquare.forEach(function (s) {
+    stage.addChild(s.label);
+});
+stage.update();
---
+++ js/play.js
@@ -0,0 +1,124 @@
+"use strict";
+
+//----------Move Player--------
+let tabTokens = [];
+board.players().forEach(function (p) {
+    tabTokens.push(circle(tabSquare[p.position()].square.x + 25, tabSquare[p.position()].square.y + 25, 7, p.color()));
+});
+
+function movePlayers() {
+    for (let i = 0; i < tabTokens.length; i++) {
+        tabTokens[i].circle.x = tabSquare[board.players()[i].position()].square.x + 25 + (i * 8);
+        tabTokens[i].circle.y = tabSquare[board.players()[i].position()].square.y + 25 + (i * 8);
+    }
+    stage.update();
+}
+
+//-------------Play--------------
+movePlayers();
+let squares = [];
+let labels = [];
+let namePlayer = new createjs.Text("", "16px Arial", "#000000");
+namePlayer.x = 250;
+namePlayer.y = 120;
+let img = new Image();
+let img2 = new Image();
+
+function drawImage(posx, posy, event) {
+    let image = event.target;
+    let bitmap = new createjs.Bitmap(image);
+    bitmap.x = posx;
+    bitmap.y = posy;
+    stage.addChild(bitmap);
+    stage.update();
+}
+
+img.onload = function (event) {
+    drawImage(200, 330, event);
+};
+img2.onload = function (event) {
+    drawImage(300, 330, event);
+};
+let test;
+play();
+
+function play() {
+    let tabAction = board.actions();
+    if (test) {
+        namePlayer.text = board.players()[board.activePlayer()].color() + " turn";
+        namePlayer.text += "\n\nYou have: " + board.players()[board.activePlayer()].money() + " monopoly";
+        if (tabAction[0].name === "buy") {
+            namePlayer.text += "\n\nThe price is: " + board.squares()[board.players()[board.activePlayer()].position()].price() + " monopoly";
+        }
+        namePlayer.color = board.players()[board.activePlayer()].color();
+    }
+    test = true;
+    squares.forEach(function (s) {
+        stage.removeChild(s);
+    });
+    labels.forEach(function (l) {
+        stage.removeChild(l);
+    });
+    squares = [];
+    for (let i = 0; i < tabAction.length; i++) {
+        let square = new createjs.Shape();
+        square.graphics.beginFill("#0B486B").drawRect(0, 0, 80, 80);
+        square.x = 210 + (i * 85);
+        square.y = 440;
+        square.on("click", function () {
+            if (tabAction[i].name === "surrender") {
+                stage.removeChild(tabTokens[board.activePlayer()].circle);
+                tabTokens.splice(board.activePlayer(), 1);
+            }
+            let ret = tabAction[i]();
+            if (ret !== undefined) {
+                if (ret.hasOwnProperty("dice1")) {
+                    img.src = "img/dice-" + ret.dice1 + "-th.png";
+                    img2.src = "img/dice-" + ret.dice2 + "-th.png";
+                } else {
+                    img.src = "";
+                    img2.src = "";
+                }
+                if (ret.text !== "") {
+                    namePlayer.text = board.players()[board.activePlayer()].color() + " turn";
+                    namePlayer.text += "\n\nYou have: " + board.players()[board.activePlayer()].money() + " monopoly";
+                    namePlayer.text += "\n\n" + ret.text;
+                    test = false;
+                }
+            }
+
+            if (tabAction[i].name === "buy") {
+                tabSquare[board.players()[board.activePlayer()].position()].circle.commandCircle.style = board.players()[board.activePlayer()].color();
+            }
+            if (tabAction[i].name === "auction") {
+                $('#auction').modal();
+            }
+
+            if (tabAction[i].name === "trade") {
+                $('#trade').modal();
+            }
+
+
+            movePlayers();
+            play();
+        });
+        stage.addChild(namePlayer);
+        stage.update();
+        let label = new createjs.Text("", "15px Arial", "#000000");
+        label.text = tabAction[i].name;
+        label.x = square.x + 20;
+        label.y = square.y + 25;
+        squares.push(square);
+        labels.push(label);
+        stage.addChild(square);
+        stage.addChild(label);
+        stage.update();
+    }
+}
+
+function finishAuction() {
+    let buyer = $('#buyer').val();
+    board.finishAuction(buyer, $('#priceAuction').val());
+    tabSquare[board.players()[board.activePlayer()].position()].circle.commandCircle.style = board.players()[buyer].color();
+    stage.update();
+}
